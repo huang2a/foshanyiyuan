a92826e9a5e9a65a729337e957af60bed0619078
huang2a <huang2a>
2016-01-19 16:56:50 +0800

    update  appointment page
    更新专家时间和预约挂号

--- js/mian.js
+++ js/mian.js
@@ -70,20 +70,24 @@ $(function() {
   }
   tabs(".tab-hd-h", "active", ".tab-bd-h");
 });
-
 $(document).ready(function(){
     var k=!0;
-
     $(".loginmask").css("opacity",0.4);
-    if($.browser.version <= 6){
-        $('#reg_setp,.loginmask').height($(document).height());
-    }
-
     $(".thirdlogin ul li:odd").css({marginRight:0});
     $(".openlogin").click(function(){
         k&&"0px"!=$("#loginalert").css("top")&& ($("#loginalert").show(),$(".loginmask").fadeIn(500),$("#loginalert").animate({top:"50%"},400,"easeOutQuart"))
     });
     $(".loginmask,.closealert").click(function(){
         k&&(k=!1,$("#loginalert").animate({top:-600},400,"easeOutQuart",function(){$("#loginalert").hide();k=!0}),$(".loginmask").fadeOut(500))
+    });
+});
+$(document).ready(function(){
+    var k=!0;
+    $(".loginmask").css("opacity",0.4);
+    $("#sidebar6").click(function(){
+        k&&"0px"!=$("#loginalert1").css("top")&& ($("#loginalert1").show(),$(".loginmask").fadeIn(500),$("#loginalert1").animate({top:"50%"},400,"easeOutQuart"))
+    });
+    $(".loginmask,.closealert").click(function(){
+        k&&(k=!1,$("#loginalert1").animate({top:-600},400,"easeOutQuart",function(){$("#loginalert1").hide();k=!0}),$(".loginmask").fadeOut(500))
     });
 });
